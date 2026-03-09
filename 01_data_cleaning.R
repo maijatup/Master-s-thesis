de964172bb4ec08941e99e518edad25d641f9e43
maijatup <maija.tupala@hotmail.com>
2026-03-09 16:16:58 +0100

    Calculate tree basal area and add to the main dataset

--- 01_data_cleaning.R
+++ 01_data_cleaning.R
@@ -1,7 +1,10 @@
 #Data cleaning
 
+install.packages("janitor")
+
 library(readr)
 library(dplyr)
+library(janitor)
 
 #Load datasets
 seedling_raw <- read_csv2("raw_data/seedling_data_raw.csv")
@@ -65,6 +68,52 @@ regeneration_data <- regeneration_data %>%
               select(site, plot, treatment, transect, subplot, pH),
             by = c("site", "plot", "treatment", "transect", "subplot")) %>% 
   select(site, plot, treatment, transect, subplot, mean_canopy_openness, pH, everything())
+
+
+#Add tree basal area to regeneration_data
+#Calculate basal area for every individual tree
+tree_ba <- tree_raw %>% 
+  mutate(basal_area = pi * (diameter_cm /100 / 2)^2)
+
+#Calculate basal area by species
+species_ba <- tree_ba %>% 
+  group_by(site, plot, transect_range, treatment, species) %>% 
+  summarise(basal_area = sum(basal_area), .groups = "drop")
+
+#Calculate total basal area
+total_ba <- tree_ba %>% 
+  group_by(site, plot, transect_range, treatment) %>% 
+  summarise(total_ba = sum(basal_area), .groups = "drop")
+
+#Convert to wide and clean names
+species_ba_wide <- species_ba %>% 
+  pivot_wider(names_from = species,
+              values_from = basal_area,
+              values_fill = 0) %>% 
+  janitor::clean_names() %>% 
+  rename_with(~ paste0(.x, "_ba"),
+              -c(site, plot, transect_range, treatment)) %>% 
+  select(site, plot, transect_range, treatment, quercus_sp_ba, everything())
+
+#Add species and stand BA to regeneration_data
+#Note: BA was only measured around one of the transects in each plot and BA values are therefore only added for these transects
+regeneration_data <- regeneration_data %>% 
+  left_join(species_ba_wide %>% select(-transect_range), 
+            by = c("site", "plot", "treatment")) %>% 
+  left_join(total_ba %>% select(-transect_range),
+            by = c("site", "plot", "treatment")) %>%
+  mutate(across(
+    ends_with("_ba"), 
+    ~ case_when(
+      site == "Karla" & transect %in% c("NV45", "SO55") ~ .,
+      site == "Rya åsar" & transect %in% c("NO80", "SV55") ~ .,
+      site == "Sandviksås" & transect %in% c("V55", "Ö45") ~ .,
+      site == "Skölvene" & transect %in% c("V55", "Ö55") ~ .,
+      site == "Östadkulle" & transect %in% c("V75", "Ö75") ~ .,
+      TRUE ~ NA_real_)))
+  
+
+
 
 
 
