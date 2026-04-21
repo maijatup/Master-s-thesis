e69aaafff48933528f37301aa336641888cb681b
maijatup <maija.tupala@hotmail.com>
2026-04-21 11:00:24 +0200

    initial commit

---
+++ 04_analysis_Q2.R
@@ -0,0 +1,8 @@
+#21/4/2026
+#Question 2: How do forest structure, light availability, and soil conditions influence oak seedling and sapling density across the study sites?
+
+
+library(readr)
+library(dplyr)
+
+regeneration <- read_csv("processed_data/regeneration_data.csv")
